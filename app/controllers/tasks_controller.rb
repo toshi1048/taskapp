7c677aea2f468535fd26eeb0d23dccbb9336611c
toshi1048 <toshiya.22.1048@gmail.com>
2020-04-09 14:13:25 +0900

    コントローラー作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+class TasksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url,notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name,:description)
+  end
+end
