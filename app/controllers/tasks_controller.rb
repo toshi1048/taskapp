1128b534daa97b97281a953014273775bcdd9064
toshi1048 <toshiya.22.1048@gmail.com>
2020-04-10 10:31:36 +0900

    編集機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,8 +18,20 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to task_url, notice: "タスク「＃{task..nam}」を更新しました。"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+  end
   private
 
   def task_params
---
+++ db/migrate/20200410012422_change_tasks_name_limit30.rb
@@ -0,0 +1,5 @@
+class ChangeTasksNameLimit30 < ActiveRecord::Migration[6.0]
+  def up
+    change_
+  end
+end
