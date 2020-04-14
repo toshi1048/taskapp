055dcf88d3b178b9ec860460f96eb601e98a2a3d
toshi1048 <toshiya.22.1048@gmail.com>
2020-04-14 11:11:05 +0900

    データ制限、ユーザー登録、削除、編集

--- db/migrate/20200410012422_change_tasks_name_limit30.rb
+++ db/migrate/20200410012422_change_tasks_name_limit30.rb
@@ -1,5 +1,8 @@
 class ChangeTasksNameLimit30 < ActiveRecord::Migration[6.0]
   def up
-    change_
+    change_column :tasks, :name, :string, limit:30
+  end
+  def down
+    change_column :tasks, :name, :string
   end
 end
