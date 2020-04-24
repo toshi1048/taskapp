3964894bec24a9612a8403f43cf71e210c2e3884
toshi1048 <toshiya.22.1048@gmail.com>
2020-04-24 19:06:48 +0900

    管理者以外でも登録可能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-  before_action :login_required
+  before_action :login_required,except:[:new,:create]
 
   private
 
