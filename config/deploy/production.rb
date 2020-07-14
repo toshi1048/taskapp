efeb547d04694f32a91094282e704ca868e81878
toshi1048 <toshiya.22.1048@gmail.com>
2020-07-14 14:10:30 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -50,7 +50,7 @@
 # The server-based syntax can be used to override options:
 # ------------------------------------
 server '18.176.70.4', user: 'admin', roles: %w{admin db web},
-set ssh_options: , keys: '/Users/fukutometoshiya/.ssh/id_rsa'
+set ssh_options, keys: '/Users/fukutometoshiya/.ssh/id_rsa'
 #{
 #     user: 'user_name', # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
