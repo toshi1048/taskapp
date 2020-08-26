9c5a1f3fcd4cbaaed6b1082842885ea74ce2dc0f
toshi1048 <toshiya.22.1048@gmail.com>
2020-08-26 15:40:21 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :keep_releases, 5
 # Rubyのバージョン
 set :rbenv_ruby, '2.5.1'
 set :rbenv_type, :user
-set :rbenv_custom_path, '/root/.rbenv'
+set :rbenv_custom_path, '/.rbenv'
 #出力するログのレベル。エラーログを詳細に見たい場合は :debug に設定する。
 #本番環境用のものであれば、 :info程度が普通。ただし挙動をしっかり確認したいのであれば :debug に設定する。
 set :log_level, :info
