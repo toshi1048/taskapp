52fe3009d4c5f21cfcd690a323bdf99b8917caf4
toshi1048 <toshiya.22.1048@gmail.com>
2020-08-27 11:14:03 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,11 @@ module Taskapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone =  'Asia/Tokyo'
-    config.assets.precompile = []
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
+
+config.assets.precompile = []
